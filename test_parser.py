a19b0117b0dddca657afbd4b29529998e6167ba5
Ruslan Forostianov <ruslan@forostianov.com>
2019-03-03 09:17:44 +0000

    Create and use assert for parsing

--- test_parser.py
+++ test_parser.py
@@ -3,46 +3,48 @@ from parser import *
 
 class Parser(unittest.TestCase):
 
-    def test_parse_empty_input(self):
-        inp = []
+    def assertParsesInto(self, inp, expected_result):
         out = list(parse(inp))
-        self.assertEqual(out, [])
+        self.assertEqual(out, expected_result)
+
+    def test_parse_empty_input(self):
+        self.assertParsesInto([], [])
 
     def test_parse_sub(self):
-        inp = [
-            'sub',
-        ]
-        out = list(parse(inp))
-        self.assertEqual(out, [
-            SubCommand(),
-        ])
+        self.assertParsesInto(
+            [
+                'sub'
+            ],
+            [
+                SubCommand(),
+            ])
 
     def test_parse_add(self):
-        inp = [
-            'add',
-        ]
-        out = list(parse(inp))
-        self.assertEqual(out, [
-            AddCommand(),
-        ])
+        self.assertParsesInto(
+            [
+                'add'
+            ],
+            [
+                AddCommand(),
+            ])
 
     def test_parse_push(self):
-        inp = [
-            'push constant 1',
-        ]
-        out = list(parse(inp))
-        self.assertEqual(out, [
-            PushCommand('constant', 1),
-        ])
+        self.assertParsesInto(
+            [
+                'push constant 1',
+            ],
+            [
+                PushCommand('constant', 1),
+            ])
 
     def test_parse_pop(self):
-        inp = [
-            'pop constant 1',
-        ]
-        out = list(parse(inp))
-        self.assertEqual(out, [
-            PopCommand('constant', 1),
-        ])
+        self.assertParsesInto(
+            [
+                'pop constant 1',
+            ],
+            [
+                PopCommand('constant', 1),
+            ])
 
 
 if __name__ == '__main__':
