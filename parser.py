ff1e70ca7e9577a82c7fe03b453ab37b924ac9f6
Ruslan Forostianov <ruslan@forostianov.com>
2019-03-03 13:21:03 +0000

    Move command classes to separate module
    To be able to reuse in writer

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
+from commands import *
+
+
 def parse(iterator):
     for command in iterator:
         command = _normalize_command(command)
@@ -25,76 +28,3 @@ def _remove_comments(command):
         indx = command.index('//')
         return command[:indx]
     return command
-
-
-class Command:
-    @classmethod
-    def find_subclass_by_command_name(cls, command_name):
-        for scls in cls.__subclasses__():
-            if scls.command_name == command_name:
-                return scls
-
-    def __repr__(self):
-        return self.__class__.__name__ + str(self.__dict__)
-
-    def __eq__(self, other):
-        return type(other) is type(self) and other.__dict__ == self.__dict__
-
-
-class Sub(Command):
-    command_name = 'sub'
-
-
-class Add(Command):
-    command_name = 'add'
-
-
-class Negate(Command):
-    command_name = 'neg'
-
-
-class Equals(Command):
-    command_name = 'eq'
-
-
-class GreaterThan(Command):
-    command_name = 'gt'
-
-
-class LessThan(Command):
-    command_name = 'lt'
-
-
-class And(Command):
-    command_name = 'and'
-
-
-class Or(Command):
-    command_name = 'or'
-
-
-class Not(Command):
-    command_name = 'not'
-
-
-segments = { 'argument', 'local', 'static', 'constant', 'this', 'that', 'pointer', 'temp' }
-
-
-class Push(Command):
-    command_name = 'push'
-
-    def __init__(self, *args):
-        if args[0] not in segments:
-            raise ValueError("Unknown segment '%s'." % args[0])
-        self.segment = args[0]
-        self.index = int(args[1])
-
-
-class Pop(Command):
-    command_name = 'pop'
-
-    def __init__(self, *args):
-        if args[0] not in segments:
-            raise ValueError("Unknown segment '%s'." % args[0])
-        self.segment = args[0]
-        self.index = int(args[1])
