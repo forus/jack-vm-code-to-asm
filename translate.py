45bf3b7ab5699359a049891f4d6a8faefaba157e
Ruslan Forostianov <foro.ru@gmail.com>
2019-03-08 18:48:46 +0100

    Fix various bugs after testing

--- translate.py
+++ translate.py
@@ -5,14 +5,17 @@ def translate(commands):
         cls = type(command)
         if cls in _command_to_translation:
             translation = _command_to_translation[cls]
+            yield '//' + str(command)
             for asm_line in translation(command):
                 yield asm_line
 
 
 def _push_command(command):
     if command.segment in _segment_to_address:
-        return _push(_segment_to_address[command.segment], command.index)
-    elif command.segment == 'constant':
+        return _push(_segment_to_address[command.segment], command.index, pointer=False)
+    if command.segment in _segment_to_pointer:
+        return _push(_segment_to_pointer[command.segment], command.index, pointer=True)
+    if command.segment == 'constant':
         return _push_constant(command.index)
     else:
         raise ValueError('Unknown segment %s' % command.segment)
@@ -20,7 +23,9 @@ def _push_command(command):
 
 def _pop_command(command):
     if command.segment in _segment_to_address:
-        return _pop(_segment_to_address[command.segment], command.index)
+        return _pop(_segment_to_address[command.segment], command.index, pointer=False)
+    if command.segment in _segment_to_pointer:
+        return _pop(_segment_to_pointer[command.segment], command.index, pointer=True)
     else:
         raise ValueError('Unknown segment %s' % command.segment)
 
@@ -28,23 +33,23 @@ def _pop_command(command):
 def _sub_command(command):
     return (_pop_from_stack_to_d_register() +
         [
-            '@R5',
+            '@R14',
             'M=D',
         ] + _pop_from_stack_to_d_register() +
         [
-            '@R5',
-            'D=M-D',
+            '@R14',
+            'D=D-M',
         ] + _push_d_register_to_stack())
 
 
 def _add_command(command):
     return (_pop_from_stack_to_d_register() +
         [
-            '@R5',
+            '@R14',
             'M=D',
         ] + _pop_from_stack_to_d_register() +
         [
-            '@R5',
+            '@R14',
             'D=D+M',
         ] + _push_d_register_to_stack())
 
@@ -57,12 +62,16 @@ _command_to_translation = {
 }
 
 
-_segment_to_address = {
+_segment_to_pointer = {
     'argument': 'ARG',
     'local': 'LCL',
-    'static': 'R16',
     'this': 'THIS',
     'that': 'THAT',
+}
+
+
+_segment_to_address = {
+    'static': 'R16',
     'temp': 'R5',
 }
 
@@ -75,18 +84,19 @@ def _push_constant(const):
     ] + _push_d_register_to_stack()
 
 
-def _push(segment, index):
+def _push(segment, index, pointer=True):
    if index == 0:
        return [
             # save value of the first segment memory cell to D register
             '@' + segment,
+            ('A=M' if pointer else ''),
             'D=M',
        ] + _push_d_register_to_stack()
    return [
         # save value of the n-th segment memory cell to D register
-        '@' + str(index),
-        'D=A',
         '@' + segment,
+        'D=' + ('M' if pointer else 'A'),
+        '@' + str(index),
         'A=D+A',
         'D=M',
    ] + _push_d_register_to_stack()
@@ -104,18 +114,18 @@ def _push_d_register_to_stack():
     ]
 
 
-def _pop(segment, index):
+def _pop(segment, index, pointer=True):
    if index == 0:
        return [
             '@' + segment,
-            'D=M',
+            'D=' + ('M' if pointer else 'A'),
             '@R13',
             'M=D',
        ] + _pop_from_stack_to_r13_ref()
    return [
-        '@' + str(index),
-        'D=A',
         '@' + segment,
+        'D=' + ('M' if pointer else 'A'),
+        '@' + str(index),
         'D=D+A',
         '@R13',
         'M=D',
